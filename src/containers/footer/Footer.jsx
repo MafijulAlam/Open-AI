7d724b238e938b1dc569beb4a2479997b149152c
MafijulAlam <mafijulcse@gmail.com>
2022-12-19 22:37:19 +0600

    second commit

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => (
       <div className="gpt3__footer-links_logo">
         <img src={gpt3Logo} alt="gpt3_logo" />
         <p>
-          Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved
+          Mafijul Alam, <br /> All Rights Reserved
         </p>
       </div>
       <div className="gpt3__footer-links_div">
@@ -36,14 +36,14 @@ const Footer = () => (
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Get in touch</h4>
-        <p>Crechterwoord K12 182 DK Alknjkcb</p>
-        <p>085-132567</p>
-        <p>info@payme.net</p>
+        <p>Mafijul Alam</p>
+        <p>+8801684674795</p>
+        <p>mafijulalam@gmail.com</p>
       </div>
     </div>
 
     <div className="gpt3__footer-copyright">
-      <p>@2021 GPT-3. All rights reserved.</p>
+      <p>@2022, All rights reserved.</p>
     </div>
   </div>
 );
