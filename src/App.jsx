f8f9f693cfad7350e0d411cd58f596f595fb4f0d
MafijulAlam <mafijulcse@gmail.com>
2022-12-19 21:22:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+
+import { Brand, CTA, Article, Feature, Navbar } from './components/index';
+import {
+  Blog,
+  Features,
+  Header,
+  Footer,
+  WhatGPT3,
+  Possibility,
+} from './containers';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
